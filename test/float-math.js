6ead8290237bb0ba4c4c0dfd0ea7856c77df177c
Alan Lu <alanlu1023@gmail.com>
2018-12-19 16:40:51 -0600

    More stuff

--- test/float-math.js
+++ test/float-math.js
@@ -1,34 +1,95 @@
-const { mpf } = require("mp-wasm")
-const FloatMath = artifacts.require("FloatMath")
+const { mpf } = require("mp-wasm");
+const { randomHex, toBN } = web3.utils;
+const FloatMath = artifacts.require("FloatMath");
 
-contract('FloatMath', function(accounts) {
-    let floatMath
+const pow2To255 = toBN(2).pow(toBN(255));
 
-    before(async function() {
-        floatMath = await FloatMath.new()
-    })
+// https://stackoverflow.com/a/50767210
+function bufferToHex(buffer) {
+  return (
+    "0x" +
+    Array.from(new Uint8Array(buffer))
+      .map(b => b.toString(16).padStart(2, "0"))
+      .join("")
+  );
+}
 
-    it('can encode and decode', async function() {
-        console.log(await floatMath.initFromInt(0))
-        console.log(await floatMath.initFromInt(10))
-        console.log(await floatMath.initFromInt(-7))
-    })
+mpf.setDefaultPrec(237);
+mpf.setDefaultRoundingMode("roundTowardZero");
+const binExpMin = -262142;
+const binExpMax = 262145;
 
-    it('operates correctly with zero', async function() {
-        const zero = await floatMath.initFromInt(0)
-        const x = await floatMath.initFromInt(10)
-        assert.equal(await floatMath.add(zero, x), x)
-        assert.equal(await floatMath.add(x, zero), x)
-        assert.equal(await floatMath.sub(x, zero), x)
-        assert.equal(await floatMath.mul(zero, x), zero)
-        assert.equal(await floatMath.mul(x, zero), zero)
-        assert.equal(await floatMath.div(zero, x), zero)
-    })
+function toBytes32(x) {
+  x = mpf(x);
+  const bs = new ArrayBuffer(32);
+  const bsView = new DataView(bs);
+  const binExp = x.getBinaryExponent();
 
-    it('can add')
-    it('can sub')
-    it('can mul')
-    it('can div')
-    it('can log2')
-    it('can get the reciprocal of the square root')
-})
+  const header =
+    ((x.isSignBitSet() ? 0x80000 : 0) |
+      ((Number.isNaN(binExp)
+        ? binExpMax
+        : Math.min(Math.max(binExp, binExpMin), binExpMax)) -
+        binExpMin)) <<
+    12;
+  bsView.setUint32(0, header);
+
+  if (Number.isNaN(binExp)) {
+    bsView.setUint8(31, 1);
+  } else if (binExp > binExpMin && binExp < binExpMax) {
+    const xBytes = x.getSignificandRawBytes();
+    const lastXByte = xBytes[xBytes.length - 1];
+    if (lastXByte & (0x80 === 0)) {
+      throw new Error(
+        `expected most significant bit of ${x} significand to be set but found ${lastXByte}`
+      );
+    }
+    bsView.setUint8(2, bsView.getUint8(2) | ((lastXByte & 0x7f) >> 3));
+    for (i = 1; i < xBytes.length && i < 30; i++) {
+      bsView.setUint8(
+        i + 2,
+        ((xBytes[xBytes.length - i] << 5) |
+          (xBytes[xBytes.length - i - 1] >> 3)) &
+          0xff
+      );
+    }
+  }
+  return bufferToHex(bs);
+}
+
+contract("FloatMath", function(accounts) {
+  let floatMath;
+
+  before(async function() {
+    floatMath = await FloatMath.new();
+  });
+
+  it("can encode and decode", async function() {
+    for (let i = 0; i < 1000; i++) {
+      const x = toBN(randomHex(32)).sub(pow2To255);
+      assert.equal(
+        await floatMath.initFromInt(x),
+        toBytes32(x),
+        `${x} produced different binary256 bytes`
+      );
+    }
+  });
+
+  it("operates correctly with zero", async function() {
+    const zero = await floatMath.initFromInt(0);
+    const x = await floatMath.initFromInt(10);
+    assert.equal(await floatMath.add(zero, x), x);
+    assert.equal(await floatMath.add(x, zero), x);
+    assert.equal(await floatMath.sub(x, zero), x);
+    assert.equal(await floatMath.mul(zero, x), zero);
+    assert.equal(await floatMath.mul(x, zero), zero);
+    assert.equal(await floatMath.div(zero, x), zero);
+  });
+
+  it("can add");
+  it("can sub");
+  it("can mul");
+  it("can div");
+  it("can log2");
+  it("can get the reciprocal of the square root");
+});
